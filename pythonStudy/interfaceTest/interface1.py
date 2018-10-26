8887bfad2286cdfb6de7ba7fbc0ac5e9325fd474
test <test@rrkd.cn>
2018-10-26 18:01:49 +0800

    requestsStudy

--- pythonStudy/interfaceTest/interface1.py
+++ pythonStudy/interfaceTest/interface1.py
@@ -1,11 +1,13 @@
 import requests
-
+import json
 url = 'http://api.douban.com/v2/movie/top250'
 r = requests.get(url, params={'start': 1, 'count': 25})
 print(r)
+print(r.url)
 res = r.json()
 for movie in res['subjects']:
     print(movie['title'])
+'''
 rp = requests.post(url, data={'start': 1, 'count': 25})
 print(rp.url)  # post
 print(r.url)   # get
@@ -22,11 +24,8 @@ print(r.status_code)  # 检查请求是否成功
 rs = requests.get(url, stream=True)
 print(rs.raw)   # 原始响应内容
 print(rs.raw.read(10))
-'''
-with open('rawtext.tiff', 'wb') as fd:
-    for chunk in rs.iter_content(chunk_size=240, decode_unicode=True):
-        fd.write(chunk)
-'''
+
+
 headers = {'user-agent': 'my-app/0.0.1'}
 r = requests.get(url, headers=headers)
 print(r.status_code)  # 响应状态码
@@ -37,4 +36,14 @@ print(bad_r.raise_for_status())
 print(r.headers)
 print(r.headers['connection'])
 
-
+'''
+dict_a = {"k1":"v1", "k2":"v2"}
+x = json.dumps(dict_a)
+print(x)
+print(type(x))
+y = json.loads(x)
+print(y)
+print(type(y))
+x1 = json.dumps(y)
+print(x1)
+print(type(x1))
--- pythonStudy/interfaceTest/interface2.py
+++ pythonStudy/interfaceTest/interface2.py
@@ -1,8 +1,12 @@
 import requests
 
 
-url = "https://sdfw.rrkd.cn/api/incidentally/shoptripbrand/brandShopDetail"
-data = {"brandId": "1", "lat": "22.547000", "lng": "114.085947", "cityName": "深圳市"}
+#url = "https://sdfw.rrkd.cn/api/incidentally/shoptripbrand/brandShopDetail"
+url = "https://sdfw.rrkd.cn/api/incidentally/shoptripbrand/shopDetail"
+
+#data = {"brandId": "1", "lat": "22.547000", "lng": "114.085947", "cityName": "深圳市"}
+data = {"shopId":172,"tripId":11479,"userId":31}
+'''
 r = requests.post(url, json=data, verify=False)
 
 print(r.status_code)
@@ -12,3 +16,15 @@ res = r.json()
 #print(r.request.headers)
 for addr in res['result']['jsonObject']['selectiveShop']:
     print(addr['destAddress'])
+
+with requests.post(url, json=data, verify=False, stream=True) as r:
+
+    res = r.json()
+    for addr in res['result']['jsonObject']['selectiveShop']:
+        print(addr['destAddress'])
+'''
+with requests.post(url, json=data, verify=False) as r:
+    res = r.json()
+    for addr in res['result']['jsonObject']['shopDetailGood']:
+        print(addr['brandName'])
+
